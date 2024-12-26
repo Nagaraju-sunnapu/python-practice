d6d894acbf2bb07ae6ee263672ee7c6f2cd8fc52
SUNNAPU NAGARAJU <sunnapunagarajuchandu@gmail.com>
2024-12-26 16:21:03 +0530

    Add files via upload

---
+++ reg12.py
@@ -0,0 +1,5 @@
+# program for demonstrating the regular expressions
+import re
+x=re.finditer("K?","KVKKVKKKVKV")
+for i in x:
+	print("start index:{},end index:{},value:{}".format(i.start(),i.end(),i.group()))
